525afc468bbbfef649597c5b055e15afa8cc063f
Radosław Kołoczek <radoslaw.koloczek@gmail.com>
2023-01-17 17:29:48 +0100

    Add pagination on articles page

--- pages/articles/index.tsx
+++ pages/articles/index.tsx
@@ -1,17 +1,113 @@
-import { FC, ReactElement } from "react";
+import { FC, ReactElement, useEffect, useState } from "react";
 import Head from "next/head";
 import React from "react";
 import { client } from "@/utils/sanityClient";
 import ArticlesContainer from "@/components/ArticlesContainer";
 import styled from "styled-components";
 import CategoryButton from "@/components/CategoryButton";
+import Pagination from "@/components/Pagination";
+import { groq } from "next-sanity";
 
 interface IProps {
 	articles: { _id: string }[];
 	categories: { _id: string; category: string }[];
+	total: number;
 }
+const resultsPerPage = 2;
 
-const ArticlesPage: FC<IProps> = ({ articles, categories }): ReactElement => {
+const ArticlesPage: FC<IProps> = ({
+	articles,
+	categories,
+	total,
+}): ReactElement => {
+	const [results, setResults] = useState<{ _id }[]>(articles);
+	const [lastId, setLastId] = useState<string | null>("");
+	const [prevId, setPrevId] = useState<string | null>("");
+	const [currentPage, setCurrentPage] = useState(0);
+	const [lastPage, setLastPage] = useState<number>(0);
+	const [prevPages, setPrevPages] = useState([]);
+
+	useEffect(() => {
+		setLastId(articles[articles.length - 1]._id);
+		setLastPage(Math.ceil(total / resultsPerPage));
+	}, []);
+
+	useEffect(() => {
+		console.log(prevPages);
+		setPrevId(prevPages[currentPage - 1]);
+	}, [prevPages, currentPage]);
+
+	const fetchNextPage = async () => {
+		setPrevId(results[0]._id);
+
+		setPrevPages((prev) => {
+			const isExist = prev.find((id) => id === results[0]._id);
+			if (!isExist) {
+				return [...prev, results[0]._id];
+			}
+			return prev;
+		});
+		try {
+			if (lastId === null) {
+				return;
+			}
+			const results = await client.fetch(
+				groq`*[_type == "article" && _id > $lastId] | order(_id) [0...${resultsPerPage}]{
+                _id, title, slug, coverImage{
+					asset->{
+						_id,
+						url
+							}
+						}
+            }`,
+				{ lastId }
+			);
+			console.log(results);
+
+			if (results.length > 0) {
+				setLastId(results[results.length - 1]._id);
+				setResults(results);
+				setCurrentPage((prev) => prev + 1);
+			} else {
+				setLastId(null);
+			}
+		} catch (err) {
+			console.log(err);
+		}
+	};
+
+	const fetchPrevPage = async () => {
+		try {
+			if (prevId === null || prevId === "") {
+				return;
+			}
+
+			const results = await client.fetch(
+				groq`*[_type == "article" && _id >= $prevId] | order(_id) [0...${resultsPerPage}]{
+                _id, title, slug, coverImage{
+					asset->{
+						_id,
+						url
+							}
+						}
+            }`,
+				{ prevId }
+			);
+			console.log(results);
+
+			if (results.length > 0) {
+				setPrevId(results[0]._id);
+				setLastId(results[results.length - 1]._id);
+				setResults(results);
+				setCurrentPage((prev) => prev - 1);
+			} else {
+				setPrevId(null);
+				setResults(articles);
+			}
+		} catch (err) {
+			console.log(err);
+		}
+	};
 	return (
 		<>
 			<Head>
@@ -23,9 +119,14 @@ const ArticlesPage: FC<IProps> = ({ articles, categories }): ReactElement => {
 						<CategoryButton key={category._id} category={category.category} />
 					))}
 				</aside>
-				<div>
-					<ArticlesContainer articles={articles} />
-					{/* pagination component */}
+				<div className='leftWrapper'>
+					<ArticlesContainer articles={results} />
+					<Pagination
+						fetchNextPage={fetchNextPage}
+						fetchPrevPage={fetchPrevPage}
+						currentPage={currentPage}
+						lastPage={lastPage}
+					/>
 				</div>
 			</Section>
 		</>
@@ -33,20 +134,24 @@ const ArticlesPage: FC<IProps> = ({ articles, categories }): ReactElement => {
 };
 
 export const getStaticProps = async () => {
-	const articles = await client.fetch(`*[_type == "article"]{
-		title, slug, coverImage{
-			asset->{
-				_id,
-				url
-			}
-		}
-	}`);
+	const articles = await client.fetch(`{
+			"items": *[_type == "article"] | order(_id) [0...${resultsPerPage}]{
+                _id, title, slug, coverImage{
+					asset->{
+						_id,
+						url
+							}
+						}
+	},
+			"total": count(*[_type == "article"]) 
+}`);
 
 	const categories = await client.fetch(`*[_type == 'category']`);
 	return {
 		props: {
-			articles,
+			articles: articles.items,
 			categories,
+			total: articles.total,
 		},
 	};
 };
@@ -71,7 +176,9 @@ const Section = styled.section`
 		order: 1;
 	}
 
-	div {
+	div.leftWrapper {
 		order: 0;
+		display: flex;
+		flex-direction: column;
 	}
 `;
