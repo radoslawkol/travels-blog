7922478f8c123ed55e89acc7c1953591f8c5c22e
Radosław Kołoczek <radoslaw.koloczek@gmail.com>
2023-01-23 17:14:50 +0100

    Add server side rendering with styled components

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,23 +1,45 @@
-import NextDocument, { Html, Head, Main, NextScript } from "next/document";
+import NextDocument, {
+	DocumentContext,
+	Html,
+	Head,
+	Main,
+	NextScript,
+} from "next/document";
+import { ServerStyleSheet } from "styled-components";
 import React from "react";
 
-type IProps = {};
-
-class Document extends NextDocument<IProps> {
-	render() {
-		return (
-			<Html>
-				<Head>
-					<link rel='shortcut icon' href='/logo.png' />
-				</Head>
-				<body>
-					<Main />
-					<NextScript />
-					<div id='modal-root'></div>
-				</body>
-			</Html>
-		);
-	}
+export default function PageDocument() {
+	return (
+		<Html>
+			<Head>
+				<link rel='shortcut icon' href='/logo.png' />
+			</Head>
+			<body>
+				<Main />
+				<NextScript />
+				<div id='modal-root'></div>
+			</body>
+		</Html>
+	);
 }
 
-export default Document;
+PageDocument.getInitialProps = async (ctx: DocumentContext) => {
+	const styledComponentsSheet = new ServerStyleSheet();
+	const originalRenderPage = ctx.renderPage;
+	try {
+		ctx.renderPage = () =>
+			originalRenderPage({
+				enhanceApp: (App) => (props) => {
+					return styledComponentsSheet.collectStyles(<App {...props} />);
+				},
+			});
+		const initialProps = await NextDocument.getInitialProps(ctx);
+		initialProps.styles = [
+			initialProps.styles,
+			styledComponentsSheet.getStyleElement(),
+		];
+		return initialProps;
+	} finally {
+		styledComponentsSheet.seal();
+	}
+};
